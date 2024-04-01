c2a1141ec84b8a36cb514e21eb00c02f4aee7d54
Simeon <simeon.anfinrud@mac.com>
2024-04-01 13:54:11 -0400

    Initial implementation.
    This is a foundational crate for a number of experiments I'm
    conducting using continuation-passing style, Reacive-X-like
    Observables and streams, composable effects, and other functional
    fun. The trick of defining a trait that maps a lifetime to a type
    using a GAT, such that a type implementing that trait can be
    passed around in generic contexts, keeps proving useful in these
    experiments so I factored out a crate that anyone can depend on.
    
    This initial implementation provides the trait `Liftor`, three
    basic implementations `Owned<T>`, `Ref<T>`, and `RefMut<T>`, and
    implementations for tuples of `Liftor`s up to size 16. This is
    enough boilerplate that it's worth keeping in a separate crate
    with no (non-dev) dependencies that is quick to compile.
    
    Documentation could use cleanup and restructuring but I'm happy
    with the code, including the tuples, tests, and handling of
    unsized types.
    
    I expect the actual code will not need to change much, though
    documentation, tutorials, and examples could certainly improve.

---
+++ src/tuples.rs
@@ -0,0 +1,422 @@
+use crate::Liftor;
+
+/// The unit type is a `Liftor` that maps any lifetime to the unit type.
+impl<'outer> Liftor<'outer> for () {
+    type In<'inner> = () where 'outer: 'inner;
+}
+
+/// A tuple of `Liftor`s is a `Liftor` of tuples.
+///
+/// This lets you use tuples of `Liftor`s as `Liftor`s themselves. For example,
+/// you can use a tuple of `Ref`s as a `Liftor` of a tuple of references, where
+/// each reference in the tuple has the lifetime of the lifetime input to the
+/// `In` associated type.
+///
+/// Tuples are supported for up to 16 elements.
+impl<'outer, A, B> Liftor<'outer> for (A, B)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+{
+    type In<'inner> = (A::In<'inner>, B::In<'inner>) where 'outer: 'inner;
+}
+
+impl<'outer, A, B, C> Liftor<'outer> for (A, B, C)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D> Liftor<'outer> for (A, B, C, D)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E> Liftor<'outer> for (A, B, C, D, E)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F> Liftor<'outer> for (A, B, C, D, E, F)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G> Liftor<'outer> for (A, B, C, D, E, F, G)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H> Liftor<'outer> for (A, B, C, D, E, F, G, H)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J, K> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J, K)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+    K: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+        K::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J, K, L> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J, K, L)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+    K: Liftor<'outer>,
+    L: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+        K::In<'inner>,
+        L::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J, K, L, M> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J, K, L, M)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+    K: Liftor<'outer>,
+    L: Liftor<'outer>,
+    M: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+        K::In<'inner>,
+        L::In<'inner>,
+        M::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J, K, L, M, N> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J, K, L, M, N)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+    K: Liftor<'outer>,
+    L: Liftor<'outer>,
+    M: Liftor<'outer>,
+    N: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+        K::In<'inner>,
+        L::In<'inner>,
+        M::In<'inner>,
+        N::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+    K: Liftor<'outer>,
+    L: Liftor<'outer>,
+    M: Liftor<'outer>,
+    N: Liftor<'outer>,
+    O: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+        K::In<'inner>,
+        L::In<'inner>,
+        M::In<'inner>,
+        N::In<'inner>,
+        O::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
+
+impl<'outer, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P> Liftor<'outer>
+    for (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P)
+where
+    A: Liftor<'outer>,
+    B: Liftor<'outer>,
+    C: Liftor<'outer>,
+    D: Liftor<'outer>,
+    E: Liftor<'outer>,
+    F: Liftor<'outer>,
+    G: Liftor<'outer>,
+    H: Liftor<'outer>,
+    I: Liftor<'outer>,
+    J: Liftor<'outer>,
+    K: Liftor<'outer>,
+    L: Liftor<'outer>,
+    M: Liftor<'outer>,
+    N: Liftor<'outer>,
+    O: Liftor<'outer>,
+    P: Liftor<'outer>,
+{
+    type In<'inner> = (
+        A::In<'inner>,
+        B::In<'inner>,
+        C::In<'inner>,
+        D::In<'inner>,
+        E::In<'inner>,
+        F::In<'inner>,
+        G::In<'inner>,
+        H::In<'inner>,
+        I::In<'inner>,
+        J::In<'inner>,
+        K::In<'inner>,
+        L::In<'inner>,
+        M::In<'inner>,
+        N::In<'inner>,
+        O::In<'inner>,
+        P::In<'inner>,
+    )
+    where
+        'outer: 'inner;
+}
